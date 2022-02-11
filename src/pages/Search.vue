f8d870c0d55983a3dfaa07dfc4f70ccba6ad881c
KL Tang <15926391+kltng@users.noreply.github.com>
2022-02-11 12:39:01 -0500

    added Algolia logo

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -8,6 +8,7 @@
         :stalled-search-delay="2000"
       >
         <ais-search-box />
+        <ais-powered-by class="justify-end" />
         <div class="grid grid-cols-1">
           <div>
             <ais-hits>
